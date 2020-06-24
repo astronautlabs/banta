1492628880f4f8f24843e221d199fd68ca627147
William Lahti <wilahti@gmail.com>
2020-06-24 01:35:42 -0700

    add missing import

--- src/firebase/firebase.module.ts
+++ src/firebase/firebase.module.ts
@@ -5,11 +5,13 @@ import { DataStore } from './datastore';
 import { AngularFireModule } from '@angular/fire';
 import { environment } from 'src/environments/environment';
 import { AngularFirestoreModule } from '@angular/fire/firestore';
+import { AngularFireAuthModule } from '@angular/fire/auth';
 
 @NgModule({
     imports: [
         AngularFireModule.initializeApp(environment.firebase),
         AngularFirestoreModule,
+        AngularFireAuthModule
         
     ],
     providers: [
