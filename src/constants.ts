cb616e2f432b4cd86b5999f2b7dfc4a9fc32e1a9
William Lahti <wilahti@gmail.com>
2021-01-27 21:26:59 -0800

    move to prod for default (for now)

--- src/constants.ts
+++ src/constants.ts
@@ -2,9 +2,9 @@ import * as path from "path";
 import * as fs from "fs";
 
 export const IS_DEV = true;
-export const GCP_PROJECT_ID = 'engagechat-dev';
+export const GCP_PROJECT_ID = 'engagechat-prod';
 
-export const LOCAL_GCP_CREDENTIALS_FILE = path.join(__dirname, '..', 'private', 'firebase', 'credentials.json');
+export const LOCAL_GCP_CREDENTIALS_FILE = path.join(__dirname, '..', 'private', 'firebase', 'credentials.prod.json');
 
 export const LOCAL_GCP_CREDENTIALS = fs.existsSync(LOCAL_GCP_CREDENTIALS_FILE) ? require(LOCAL_GCP_CREDENTIALS_FILE) : undefined;
-export const FIREBASE_FIRESTORE_URL : string = "https://engagechat-dev.firebaseio.com";
+export const FIREBASE_FIRESTORE_URL : string = "https://engagechat-prod.firebaseio.com";
