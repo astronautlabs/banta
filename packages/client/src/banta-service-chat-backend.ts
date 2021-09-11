8c2e7a50c193a9fe0d7b523485c7de1cbfebb169
William Lahti <wilahti@gmail.com>
2021-09-11 14:12:45 -0700

    expose the underlyingChatBackend for convenience

--- packages/client/src/banta-service-chat-backend.ts
+++ packages/client/src/banta-service-chat-backend.ts
@@ -56,21 +56,25 @@ export class BantaServiceChatSource implements ChatSource {
 
 export class BantaServiceChatBackend implements ChatBackend {
     constructor(
-        private underlyingChatBackend : ChatBackend
+        private _underlyingChatBackend : ChatBackend
     ) {
     }
 
+    get underlyingChatBackend() {
+        return this._underlyingChatBackend;
+    }
+
     get notificationsChanged(): Observable<Notification[]> {
-        return this.underlyingChatBackend.notificationsChanged;
+        return this._underlyingChatBackend.notificationsChanged;
     }
 
     get newNotification(): Observable<Notification> {
-        return this.underlyingChatBackend.newNotification;
+        return this._underlyingChatBackend.newNotification;
     }
 
     async getSourceForTopic(topicId: string): Promise<ChatSource> {
         return new BantaServiceChatSource(
-            await this.underlyingChatBackend.getSourceForTopic(topicId),
+            await this._underlyingChatBackend.getSourceForTopic(topicId),
             this,
             `/topics/${topicId}/messages`
         );
@@ -78,26 +82,26 @@ export class BantaServiceChatBackend implements ChatBackend {
 
     async getSourceForThread(topicId: string, messageId: string): Promise<ChatSource> {
         return new BantaServiceChatSource(
-            await this.underlyingChatBackend.getSourceForThread(topicId, messageId),
+            await this._underlyingChatBackend.getSourceForThread(topicId, messageId),
             this,
             `/topics/${topicId}/messages/${messageId}`
         );
     }
 
     async refreshMessage(message: ChatMessage): Promise<ChatMessage> {
-        return await this.underlyingChatBackend.refreshMessage(message);
+        return await this._underlyingChatBackend.refreshMessage(message);
     }
 
     async getMessage(topicId: string, messageId: string): Promise<ChatMessage> {
-        return await this.underlyingChatBackend.getMessage(topicId, messageId);
+        return await this._underlyingChatBackend.getMessage(topicId, messageId);
     }
 
     async getSubMessage(topicId: string, parentMessageId: string, messageId: string): Promise<ChatMessage> {
-        return await this.underlyingChatBackend.getSubMessage(topicId, parentMessageId, messageId);
+        return await this._underlyingChatBackend.getSubMessage(topicId, parentMessageId, messageId);
     }
 
     watchMessage(message: ChatMessage, handler: (message: ChatMessage) => void): () => void {
-        return this.underlyingChatBackend.watchMessage(message, handler);
+        return this._underlyingChatBackend.watchMessage(message, handler);
     }
     
     public baseUrl : string;
