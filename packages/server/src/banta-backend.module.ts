29c5c8384afb61d5a9d34ebc7a54dd9e09290735
William Lahti <wilahti@gmail.com>
2021-09-16 12:37:25 -0700

    BantaBackendModule must be a Module not a WebService

--- packages/server/src/banta-backend.module.ts
+++ packages/server/src/banta-backend.module.ts
@@ -1,9 +1,10 @@
 import { WebService, Get, Controller, Mount } from '@alterior/web-server';
+import { Module } from '@alterior/di';
 import { ChatModule } from './chat/chat.module';
 import * as express from 'express';
 import { AccountsModule } from './accounts';
 
-@WebService({
+@Module({
     imports: [
         AccountsModule,
         ChatModule
