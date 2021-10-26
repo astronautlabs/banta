95bcaca101573938694e262bbfd385834ad171a4
William Lahti <wilahti@gmail.com>
2021-10-26 14:31:28 -0700

    test server stuff

--- packages/test-server/src/main.ts
+++ packages/test-server/src/main.ts
@@ -2,10 +2,13 @@ import "@alterior/platform-nodejs";
 import { Application } from "@alterior/runtime";
 import { WebService, Mount, Get } from "@alterior/web-server";
 import { NotificationsProvider, AuthenticationProvider } from "@banta/common";
-import { FirebaseNotificationsProvider, FirebaseAuthenticationProvider, FirebaseChatBackend } from "@banta/firebase";
+import { FirebaseNotificationsProvider, FirebaseAuthenticationProvider, FirebaseChatBackend, FirebaseStoreRef } from "@banta/firebase";
 import { ChatBackendService, BantaBackendModule, BantaBackendController } from "@banta/server";
 
 import * as Fb from 'firebase-admin';
+import { CORSMiddleware } from "./cors";
+
+globalThis.fetch = require('node-fetch');
 
 let firebaseCredsFile = process.env['FIREBASE_CREDS_FILE'] || "../private/firebase-service-account.json";
 console.log(`Loading firebase credentials from ${firebaseCredsFile}`);
@@ -16,7 +19,12 @@ Fb.initializeApp({
 });
 
 @WebService({
+    server: {
+        port: 3422,
+        middleware: [ CORSMiddleware ]
+    },
     providers: [
+        { provide: FirebaseStoreRef, useClass: FirebaseStoreRef },
         { provide: NotificationsProvider, useClass: FirebaseNotificationsProvider },
         { provide: AuthenticationProvider, useClass: FirebaseAuthenticationProvider },
         { provide: ChatBackendService, useClass: FirebaseChatBackend }
