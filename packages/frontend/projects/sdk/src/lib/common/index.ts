be9fedf1b6dc36e98454143a70c06d9c14c824b6
William Lahti <wilahti@gmail.com>
2022-08-24 18:40:41 -0700

    add missing exports

--- packages/frontend/projects/sdk/src/lib/common/index.ts
+++ packages/frontend/projects/sdk/src/lib/common/index.ts
@@ -2,5 +2,8 @@ export * from './lazy-connection';
 export * from './timestamp.component';
 export * from './lightbox/lightbox.component';
 export * from './markdown-to-html.pipe';
+export * from './trust-resource-url.pipe';
+export * from './attachment/attachment.component';
+export * from './attachments/attachments.component';
 
 export * from './common.module';
