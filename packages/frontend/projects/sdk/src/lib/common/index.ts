cf53d71c3752469b5b3022ca217fc2858aab6306
William Lahti <wilahti@gmail.com>
2022-12-24 02:41:00 -0800

    add missing export

--- packages/frontend/projects/sdk/src/lib/common/index.ts
+++ packages/frontend/projects/sdk/src/lib/common/index.ts
@@ -3,6 +3,7 @@ export * from './timestamp.component';
 export * from './lightbox/lightbox.component';
 export * from './markdown-to-html.pipe';
 export * from './trust-resource-url.pipe';
+export * from './mention-linker.pipe';
 export * from './attachment/attachment.component';
 export * from './attachments/attachments.component';
 
--- packages/frontend/projects/sdk/src/lib/index.ts
+++ packages/frontend/projects/sdk/src/lib/index.ts
@@ -16,6 +16,5 @@ export * from './youtube-attachments';
 export * from './giphy-attachments';
 export * from './tweet-attachments';
 export * from './message-menu-item';
-export * from './attachment-scraper';
 
 export * from './banta-sdk.module';
