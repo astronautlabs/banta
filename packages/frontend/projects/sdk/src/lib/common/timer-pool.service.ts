30d82da5ad796721fdbc8bc54860d2cef891c6e3
William Lahti <wilahti@gmail.com>
2023-06-13 14:18:40 -0700

    add better debugging capability to the timerpool

--- packages/frontend/projects/sdk/src/lib/common/timer-pool.service.ts
+++ packages/frontend/projects/sdk/src/lib/common/timer-pool.service.ts
@@ -16,6 +16,12 @@ interface TimerState {
 export class TimerPool {
     private subscriptions = new Map<number, TimerState>();
 
+    private newSubscriptionsNotice;
+    private newSubscriptions = new Map<number, number>();
+
+    private removedSubscriptionsNotice;
+    private removedSubscriptions = new Map<number, number>();
+
     addTimer(interval: number, callback: () => void) {
         if (interval <= 0) {
             console.warn(`Refusing to set timer with interval of ${interval}!`);
@@ -23,10 +29,14 @@ export class TimerPool {
         }
 
         let state: TimerState;
+        let sizeWas = this.subscriptions.size;
 
         if (!this.subscriptions.has(interval)) {
             state = { subscribers: [] };
-            state.handle = setInterval(() => state.subscribers.forEach(sub => sub()), interval);
+            state.handle = setInterval(() => {
+                console.debug(`[Banta/TimerPool] Notifying ${state.subscribers.length} subs [${interval}ms]`);
+                state.subscribers.forEach(sub => sub());
+            }, interval);
             this.subscriptions.set(interval, state);
         } else {
             state = this.subscriptions.get(interval);
@@ -34,9 +44,34 @@ export class TimerPool {
 
         state.subscribers.push(callback);
 
+        // Debug information //////////////////////////
+        //
+
+        if (!this.newSubscriptions.has(interval))
+            this.newSubscriptions.set(interval, 0);
+        this.newSubscriptions.set(interval, (this.newSubscriptions.get(interval) ?? 0) + 1);
+
+        clearTimeout(this.newSubscriptionsNotice);
+        this.newSubscriptionsNotice = setTimeout(() => {
+            for (let [interval, count] of this.newSubscriptions) {
+                console.debug(`[Banta/TimerPool] ${count} new subscriptions to ${interval}ms [${state.subscribers.length} total]`);
+            }
+
+            this.newSubscriptions.clear();
+        });
+
+        //
+        ///////////////////////////////////////////////
+
+        if (sizeWas === 0) {
+            console.debug(`[Banta/TimerPool] No longer idle.`);
+        }
+
+        // Unsubscribe function 
+
         return () => {
             let state = this.subscriptions.get(interval);
-            let index = state.subscribers.findIndex(callback);
+            let index = state.subscribers.indexOf(callback);
             if (index >= 0)
                 state.subscribers.splice(index, 1);
 
@@ -44,6 +79,25 @@ export class TimerPool {
                 clearInterval(state.handle);
                 this.subscriptions.delete(interval);
             }
+
+            if (!this.removedSubscriptions.has(interval))
+                this.removedSubscriptions.set(interval, 0);
+            this.removedSubscriptions.set(interval, (this.removedSubscriptions.get(interval) ?? 0) + 1);
+
+            // Debug information ////////////////////////////////////////////////////////////////////
+
+            clearTimeout(this.removedSubscriptionsNotice);
+            this.removedSubscriptionsNotice = setTimeout(() => {
+                for (let [interval, count] of this.removedSubscriptions) {
+                    let state = this.subscriptions.get(interval);
+                    console.debug(`[Banta/TimerPool] ${count} unsubscribed from ${interval}ms [${state?.subscribers?.length ?? 0} remain]`);
+                }
+
+                if (this.subscriptions.size === 0)
+                    console.debug(`[Banta/TimerPool] All subscriptions have been removed. Now idle.`);
+
+                this.removedSubscriptions.clear();
+            });
         };
     }
 }
